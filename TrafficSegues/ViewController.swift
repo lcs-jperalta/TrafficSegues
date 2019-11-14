aba0a7bbbac9667773142a5f1a927c169182b38e
Joven Peralta <jperalta@lcs.on.ca>
2019-11-13 20:17:18 -0500

    Made an example of information transferring from one view controller to another.

--- TrafficSegues/ViewController.swift
+++ TrafficSegues/ViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: Properties
+    @IBOutlet weak var textField: UITextField!
+    
+    // MARK: Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -18,6 +22,10 @@ class ViewController: UIViewController {
     @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
         
     }
-
+    // Get ready to segue to the yellow view controller
+    // Is called automatically when a segue is about to happen
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = textField.text
+    }
 }
 
